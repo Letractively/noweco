feda39b9926e7e6fc8404b8d3c87a71af2c343eb
gael.lalire <gael.lalire@1ea220df-830e-6a2d-8228-19c01309f61c>
2011-05-07 21:56:14 +0000

    use sudo -E instead of passing each system variable

--- trunk/distrib/src/main/assembly/install-noweco-bash.sh
+++ trunk/distrib/src/main/assembly/install-noweco-bash.sh
@@ -8,7 +8,7 @@ fi
 # create noweco.bash_profile
 echo '_noweco() { local cur; cur=${COMP_WORDS[COMP_CWORD]}; COMPREPLY=($(compgen -W "start stop restart" -- $cur )); }' > "$DIRNAME/noweco.bash_profile"
 echo 'complete -F _noweco noweco' >> "$DIRNAME/noweco.bash_profile"
-echo alias noweco="'"sudo env JAVA_HOME='"$JAVA_HOME"' JAVA_OPTS='"$JAVA_OPTS"' NOWECO_DEBUG='"$NOWECO_DEBUG"' '"'$DIRNAME/noweco.sh'"'"'" >> "$DIRNAME/noweco.bash_profile"
+echo alias noweco="'"sudo -E '"'$DIRNAME/noweco.sh'"'"'" >> "$DIRNAME/noweco.bash_profile"
 
 # add (or replace) inclusion of noweco.bash_profile in ~/.bash_profile
 sed -i .noweco-install-backup '$a\
