7bf5c3086b10b0b69923e11b8bc5dfd704380981
gael.lalire <gael.lalire@1ea220df-830e-6a2d-8228-19c01309f61c>
2011-05-07 19:20:19 +0000

    [maven-release-plugin]  copy for tag noweco-0.0.5

---
+++ tags/noweco-0.0.5/modules/core/src/main/java/com/googlecode/noweco/core/pop/spi/Message.java
@@ -0,0 +1,25 @@
+package com.googlecode.noweco.core.pop.spi;
+
+import java.io.IOException;
+import java.io.Reader;
+
+public interface Message {
+
+    int getId();
+
+    int getSize() throws IOException;
+
+    void setMarkedForDeletion(boolean markedForDeletion);
+
+    boolean isMarkedForDeletion();
+
+    // realy delete messages
+    void update() throws IOException;
+
+    Reader getContent() throws IOException;
+
+    Reader getHeaders() throws IOException;
+
+    String getUID() throws IOException;
+
+}
---
+++ tags/noweco-0.0.5/modules/core/src/main/java/com/googlecode/noweco/core/seam/WebmailMessage.java
@@ -0,0 +1,56 @@
+package com.googlecode.noweco.core.seam;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+
+import com.googlecode.noweco.core.pop.spi.Message;
+
+public class WebmailMessage implements Message {
+
+    private com.googlecode.noweco.core.webmail.Message webmailMessage;
+
+    private int id;
+
+    private boolean markedForDeletion = false;
+
+    public WebmailMessage(int id, com.googlecode.noweco.core.webmail.Message webmailMessage) {
+        this.id = id;
+        this.webmailMessage = webmailMessage;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getSize() throws IOException {
+        return webmailMessage.getSize();
+    }
+
+    public Reader getContent() throws IOException {
+        return new StringReader(webmailMessage.getContent());
+    }
+
+    public void setMarkedForDeletion(boolean markedForDeletion) {
+        this.markedForDeletion = markedForDeletion;
+    }
+
+    public boolean isMarkedForDeletion() {
+        return markedForDeletion;
+    }
+
+    public void update() throws IOException {
+        if (markedForDeletion) {
+            webmailMessage.delete();
+        }
+    }
+
+    public Reader getHeaders() throws IOException {
+        return new StringReader(webmailMessage.getHeader());
+    }
+
+    public String getUID() throws IOException {
+        return webmailMessage.getUniqueID();
+    }
+
+}
