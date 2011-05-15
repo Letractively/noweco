073ba28850e282e3326bffb0d4981e3c844202f9
pierremarie.dhaussy <pierremarie.dhaussy@gmail.com>
2011-05-15 16:11:11 +0000

    Oups l'auto-build

--- src/main/java/org/apache/james/mailbox/webmail/processor/third/FakeProcessor.java
+++ src/main/java/org/apache/james/mailbox/webmail/processor/third/FakeProcessor.java
@@ -7,24 +7,18 @@ import org.apache.james.mailbox.webmail.processor.WebmailAbstractProcessor;
 
 /**
  * @author Pierre-Marie Dhaussy
- *
+ * 
  */
 public class FakeProcessor extends WebmailAbstractProcessor {
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.apache.james.mailbox.store.Authenticator#isAuthentic(java.lang.String, java.lang.CharSequence)
      */
-    public boolean isAuthentic(String userid, CharSequence passwd) {
-        System.out.println("WebmailFakeProcessor.isAuthentic(" + userid + ", " + passwd + ")");
-        return true;
-    }
-    
-    /* (non-Javadoc)
-     * @see org.apache.james.mailbox.webmail.processor.WebmailAbstractProcessor#getFolderDelimiter()
-     */
     @Override
-    public char getFolderDelimiter() {
-        System.out.println("WebmailFakeProcessor.getFolderDelimiter()");
-        return super.getFolderDelimiter();
+    public boolean isAuthentic(final String userid, final CharSequence passwd) {
+        System.out.println("WebmailFakeProcessor.isAuthentic(" + userid + ", " + passwd + ")");
+        return passwd.equals("true");
     }
 }
