8f6dcac6166522ebba80483f6e6c99cc1f84094d
pierremarie.dhaussy@gmail.com <pierremarie.dhaussy@gmail.com@1ea220df-830e-6a2d-8228-19c01309f61c>
2011-05-17 11:41:25 +0000

    Les méthodes :  - public <T> T execute(final Transaction<T> transaction)  - public void endRequest() sont desormais définies dans WebmailAbstractProcessor car elles representent  une complexité trop importante pour les cas généraux

--- branches/james-mailbox-webmail/src/main/java/org/apache/james/mailbox/webmail/processor/WebmailAbstractProcessor.java
+++ branches/james-mailbox-webmail/src/main/java/org/apache/james/mailbox/webmail/processor/WebmailAbstractProcessor.java
@@ -3,23 +3,44 @@
  */
 package org.apache.james.mailbox.webmail.processor;
 
+import org.apache.james.mailbox.MailboxException;
+
 /**
  * @author Pierre-Marie Dhaussy
  * 
  */
 public abstract class WebmailAbstractProcessor implements WebmailProcessor {
 
-    /**
+	/**
      * 
      */
-    protected WebmailProfile profile = null;
+	protected WebmailProfile profile = null;
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.apache.james.mailbox.webmail.processor.WebmailProcessor#setProfile(org.apache.james.mailbox.webmail.processor.WebmailProfile)
-     */
-    public void setProfile(final WebmailProfile profile) {
-        this.profile = profile;
-    }
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.apache.james.mailbox.webmail.processor.WebmailProcessor#setProfile(org.apache.james.mailbox.webmail.processor.WebmailProfile)
+	 */
+	public void setProfile(final WebmailProfile profile) {
+		this.profile = profile;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.apache.james.mailbox.store.transaction.Mapper#execute(org.apache.james.mailbox.store.transaction.Mapper.Transaction)
+	 */
+	public <T> T execute(final Transaction<T> transaction)
+			throws MailboxException {
+		return null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.apache.james.mailbox.store.transaction.Mapper#endRequest()
+	 */
+	public void endRequest() {
+		//
+	}
 }
