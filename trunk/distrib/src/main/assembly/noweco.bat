387b2dee12546b9876f96a424969299bc7bbcbf0
gael.lalire <gael.lalire@1ea220df-830e-6a2d-8228-19c01309f61c>
2011-05-17 10:46:07 +0000

    Fix issue 10

--- trunk/distrib/src/main/assembly/noweco.bat
+++ trunk/distrib/src/main/assembly/noweco.bat
@@ -6,7 +6,7 @@ set DIRNAME=%~dp0
 
 if "%DIRNAME:~-1%" == "\" (set DIRNAME=%DIRNAME:~0,-1%)
 
-if "%JAVA_HOME%" == "" (set JAVA=java) else (set JAVA=%JAVA_HOME%\bin\java.exe)
+if "%JAVA_HOME%" == "" (set JAVA=java.exe) else (set JAVA=%JAVA_HOME%\bin\java.exe)
 
 set CLASSPATH=
 for %%j in ("%DIRNAME%\lib\*.jar") do (
@@ -17,4 +17,4 @@ if "%NOWECO_DEBUG%" neq "" (set JAVA_OPTS=%JAVA_OPTS% -Xdebug -Xnoagent -Djava.c
   
 setLocal DisableDelayedExpansion
 
-%JAVA% %JAVA_OPTS% -cp "%CLASSPATH%" -Dlogback.configurationFile="%DIRNAME%\logback.xml" com.googlecode.noweco.cli.NowecoCLI "%DIRNAME%" 1>"%DIRNAME%\logs\out.console" 2>"%DIRNAME%\logs\err.console"
+"%JAVA%" %JAVA_OPTS% -cp "%CLASSPATH%" -Dlogback.configurationFile="%DIRNAME%\logback.xml" com.googlecode.noweco.cli.NowecoCLI "%DIRNAME%" 1>"%DIRNAME%\logs\out.console" 2>"%DIRNAME%\logs\err.console"
